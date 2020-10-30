3b428613b139abf88af8bb116f2bdcf8617b131b
TakamasaSaito <takamasa16551@gmail.com>
2020-10-30 23:51:11 +0900

    Add files via upload
    add file

---
+++ Warikanproject/warikan/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import WarikanModel, MemberModel, DetailModel
+
+admin.site.register(WarikanModel)
+admin.site.register(MemberModel)
+admin.site.register(DetailModel)
---
+++ Warikanproject/warikan/apps.py
@@ -0,0 +1,4 @@
+from django.apps import AppConfig
+
+class WarikanConfig(AppConfig):
+    name = 'warikan'
